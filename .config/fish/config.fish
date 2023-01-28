8d0d52e83a9ae8e4c5871b9f8979a1fbdfaf4fd1
Kimhyr <kingemhyr7210@gmail.com>
2023-01-28 10:44:03 -0500

    .

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+alias hx=helix
+alias starthl=~/.local/bin/starthl.sh
+
+export ZIG_LOCAL_CACHE_DIR=build/cache
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
